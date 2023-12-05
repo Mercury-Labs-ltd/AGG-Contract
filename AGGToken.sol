15bd5d21fa1485037bb3dffa09b15904caabf687
Mercury Labs <149057688+Mercury-Labs-ltd@users.noreply.github.com>
2023-12-05 14:55:37 +0900

    Update AGGToken.sol
    Access Contol, MAX_SUPPLY add

--- AGGToken.sol
+++ AGGToken.sol
@@ -1,24 +1,37 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Pausable.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
 import "./Ownable.sol";
 import "./TimeLock.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Pausable.sol";
 
-contract AGGToken is ERC20Pausable, Ownable, TimeLock {
-    constructor(string memory _name, string memory _symbol, uint256 _initialSupply) ERC20(_name, _symbol) {
-        _mint(msg.sender, _initialSupply);
+contract AGGToken is ERC20Pausable, AccessControl {
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+    bytes32 public constant BURNER_ROLE = keccak256("BURNER_ROLE");
+
+    uint256 public constant MAX_SUPPLY = 10000000000000000000000000000;
+
+    constructor(string memory _name, string memory _symbol) ERC20(_name, _symbol) {
+        _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
     }
 
-    function burn(uint256 _amount) public onlyOwner {
+    function mint(address to, uint256 amount) public {
+        require(hasRole(MINTER_ROLE, msg.sender), "Caller is not a minter");
+        require(amount + totalSupply() <= MAX_SUPPLY, "Insufficient supply");
+        _mint(to, amount);
+    }
+
+    function burn(uint256 _amount) public {
+        require(hasRole(BURNER_ROLE, msg.sender), "Caller is not a burner");
         _burn(msg.sender, _amount);
     }
 
-    function pause() public onlyOwner {
+    function pause() public onlyRole(DEFAULT_ADMIN_ROLE) {
         _pause();
     }
 
-    function unpause() public onlyOwner {
+    function unpause() public onlyRole(DEFAULT_ADMIN_ROLE) {
         _unpause();
     }
 
@@ -26,7 +39,7 @@ contract AGGToken is ERC20Pausable, Ownable, TimeLock {
         address _from,
         address _to,
         uint256 _amount
-    ) internal override(ERC20Pausable) whenNotPaused notLocked {
+    ) internal override(ERC20Pausable) whenNotPaused {
         super._beforeTokenTransfer(_from, _to, _amount);
     }
 }
